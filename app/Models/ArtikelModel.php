9bb767f6177bd59e4d1dfd15fc14f0145d723d59
94gin <garin.kra@gmail.com>
2020-07-05 17:30:21 +0700

    penyesuaian model

--- app/Models/ArtikelModel.php
+++ app/Models/ArtikelModel.php
@@ -7,30 +7,32 @@ use Illuminate\Support\Str;
 class ArtikelModel {
     public static function get_all(){
         $artikel = DB::table('articles')->get();
-        $artikel += ['slug' => Str::slug($artikel['judul'],"-")];
         return $artikel;
     }
 
     public static function save($data){
+        $slug = Str::slug($data['judul'],"-");
+        $data += ['slug' => $slug];
         $artikel = DB::table('articles')->insert($data);
         return $artikel;
     }
 
     public static function find_by_id($id)
     {
-        $artikel = DB::table('articles')->where('id',$id)->get();
+        $artikel = DB::table('articles')->where('id',$id)->first();
         return $artikel;
     }
 
     public static function update($id, $request)
     {
+        $slug = Str::slug($request['judul'],"-");
         $artikel = DB::table('articles')
                         ->where('id',$id)
                         ->update([
                             'judul' => $request['judul'],
                             'isi'   => $request['isi'],
-                            'slug'   => Str::slug($request['judul'],"-"),
-                            'tag'   => $request['tag'],
+                            'slug'   => $slug,
+                            'tag'   => $request['tag']
                         ]);
         return $artikel;
     }
